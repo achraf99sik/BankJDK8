77fde7fc75a37eb3c5cc59eebe9ae156ba1a31a0
achraf <achrafsikal@gmail.com>
2025-09-19 15:24:59 +0100

    feat: update Main class to implement a menu-driven interface for account management operations.

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,43 @@
-import java.util.HashMap;
+import Controller.MenuController;
+import Service.CompteCreatorService;
+import Service.OperationService;
+
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args){
-        HashMap<String, String> capitalCities = new HashMap<>();
-        capitalCities.put("England", "London");
-        System.out.println(capitalCities.get("England"));
+        CompteCreatorService compteCreatorService = new CompteCreatorService();
+        OperationService operationService = new OperationService(compteCreatorService);
+        MenuController menu = new MenuController(compteCreatorService, operationService);
+        Scanner input = new Scanner(System.in);
+        while (true) {
+            System.out.print("\nMenu \n1 : Creer un compte\n2 : Fair un operation\n3 : Consulter solde\n4 : Afficher details compte\n5 : Lister les operations\n6 : Quitter");
+            System.out.print("\nChoisissez un option: ");
+            int option = input.nextInt();
+            switch (option){
+                case 1:
+                    menu.creerCompte();
+                    break;
+                case 2:
+                    menu.executerOperation();
+                    break;
+                case 3:
+                    menu.consulterSolde();
+                    break;
+                case 4:
+                    menu.afficherDetailsCompte();
+                    break;
+                case 5:
+                    menu.listerOperations();
+                    break;
+                case 6:
+                    System.out.println("Au revoir");
+                    input.close();
+                    return;
+                default:
+                    System.out.println("Option non valide Veuillez reessayer");
+                    break;
+            }
+        }
     }
 }
